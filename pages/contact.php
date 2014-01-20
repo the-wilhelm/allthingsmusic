8403fc6638d6f3ec3e3cd81f2b360f8f4a4742a8
the-wilhelm <the.wilhelm@gmail.com>
2014-01-20 15:52:10 -0600

    Contact Us Page

--- pages/contact.php
+++ pages/contact.php
@@ -5,4 +5,30 @@
             <p>Give us a call, send us a message, or just leave some feedback</p>
         </div>
     </div>
+    <div class="content">
+        <div class="container">
+            <div class="col-md-6">
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-sm-2 control-label">Message</label>
+                        <div class="col-sm-10">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
