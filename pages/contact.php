dfb7a7b13d5f85ea181ffabbe2aa4d3b3adc63e2
the-wilhelm <the.wilhelm@gmail.com>
2014-02-04 12:16:54 -0600

    Contact and Tuition page

--- pages/contact.php
+++ pages/contact.php
@@ -5,29 +5,62 @@
             <p>Give us a call, send us a message, or just leave some feedback</p>
         </div>
     </div>
-    <div class="content">
+    <div class="content" style="margin-bottom:40px;">
         <div class="container">
-            <div class="col-md-6">
-                <form class="form-horizontal" role="form">
-                    <div class="form-group">
-                        <label for="inputName" class="col-sm-2 control-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputName" placeholder="Name">
-                        </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jumbotron">
+                        <h1>We would love to hear from you!</h1>
+                        <p>Please dont be afraid to contact us about questions, concerns, or feedback that you have.</p>
+                        <p>
+                            <a href="tel:+9548957336" class="btn btn-danger btn-lg" role="button">Call Us</a>
+                            <a href="mailto:jesse@allthingsmusicllc.com" class="btn btn-danger btn-lg" role="button">Email Us</a>
+                        </p>
                     </div>
-                    <div class="form-group">
-                        <label for="inputEmail" class="col-sm-2 control-label">Email</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label for="inputName" class="col-sm-2 control-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputName" placeholder="Name">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputEmail" class="col-sm-2 control-label">Message</label>
-                        <div class="col-sm-10">
-                            <textarea class="form-control" rows="3"></textarea>
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                            </div>
                         </div>
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-sm-2 control-label">Message</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-danger pull-right">Submit</button>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                      <!-- Default panel contents -->
+                      <div class="panel-heading">Contact Info</div>
+                    
+                      <!-- Table -->
+                      <table class="table">
+                        <tr>
+                            <td>Phone:</td>
+                            <td>954-895-7336</td>
+                        </tr>
+                          <tr>
+                            <td>Email:</td>
+                            <td>jesse@allthingsmusicllc.com</td>
+                        </tr>
+                      </table>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
--- pages/tuition.php
+++ pages/tuition.php
@@ -5,4 +5,16 @@
             <p>Sign up or pay your tuition for us here</p>
         </div>
     </div>
+    <div class="content">
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Pay your tuition online!</h1>
+                <p>We have made it easy for you to pay your tuition online by using paypal.</p>
+                <p>
+                    <a href="images/tuition.pdf" target="_blank" class="btn btn-danger btn-lg" role="button">Download Contract</a>
+                    <a href="#" class="btn btn-danger btn-lg" role="button">Pay Online</a>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
